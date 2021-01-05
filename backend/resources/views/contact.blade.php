61ea9770746b2ee83a2c178a0bd862b048e35b23
Yuki Mizuta <nguen1225@gmail.com>
2021-01-05 23:40:15 +0900

    controller作成、ページ表示

---
+++ backend/resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<hi>contact</h1>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'App\Http\Controllers\PagesController@getHome');
+Route::get('/about', 'App\Http\Controllers\PagesController@getAbout');
+Route::get('/contact', 'App\Http\Controllers\PagesController@getContact');
